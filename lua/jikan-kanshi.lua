f102ec0f49dd887fa353a379303a72dfa6be2fc3
Stoyko Tolev <st.tolev@gmail.com>
2024-05-27 23:56:47 +0300

    feat(init): Init repo with basic structure

---
+++ lua/jikan-kanshi.lua
@@ -0,0 +1,33 @@
+local augroup = vim.api.nvim_create_augroup("JikanKanshi", { clear = true })
+
+local function bufEnter()
+    print("Entering a buffer")
+end
+
+local function bufLeave()
+    print("Leaving buffer")
+end
+
+local function setup()
+    vim.api.nvim_create_autocmd(
+        "BufEnter",
+        {
+            group = augroup,
+            desc = "Start the duration timer for the open filetype",
+            once = true,
+            callback = bufEnter,
+        }
+    )
+
+    vim.api.nvim_create_autocmd(
+        "BufLeave",
+        {
+            group = augroup,
+            desc = "End the duration timer for the open filetype",
+            once = true,
+            callback = bufLeave,
+        }
+    )
+end
+
+return { setup = setup }
