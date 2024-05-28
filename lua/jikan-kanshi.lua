b4cf5e4cc46340b7c4295b4034d79c7c64cdc5ad
Stoyko Tolev <st.tolev@gmail.com>
2024-05-28 22:56:16 +0300

    feat(*): Create file if it doesn't exist. Get the filetype and write to a file once vim is closed

--- lua/jikan-kanshi.lua
+++ lua/jikan-kanshi.lua
@@ -1,22 +1,53 @@
+local Path = require("plenary.path")
 local augroup = vim.api.nvim_create_augroup("JikanKanshi", { clear = true })
 
+local data_path = string.format("%s/jikan-kanshi", vim.fn.stdpath("data"))
+local data_path_exists = false
+local data_file
+
+local function ensure_data_path_exists()
+    if data_path_exists then
+        return
+    end
+
+    local path = Path:new(data_path)
+    if not path:exists() then
+        path:mkdir()
+    end
+    data_path_exists = true
+    return string.format("%s/filetype.txt", data_path)
+end
+
+local startTime
+local endTime
+local fileType
+
 local function bufEnter()
-    print("Entering a buffer")
+    fileType = vim.bo.filetype
+    startTime = os.time()
 end
 
 local function bufLeave()
-    print("Leaving buffer")
+    endTime = os.time()
+    local timeSpent = os.difftime(endTime, startTime)
+
+    Path:new(data_file)
+        :write(
+            fileType .. " has been opened for " .. timeSpent .. " seconds.",
+            "a"
+        )
 end
 
 local function setup()
-    vim.api.nvim_create_autocmd("FileType", {
+    data_file = ensure_data_path_exists()
+    vim.api.nvim_create_autocmd("BufNew", {
         group = augroup,
         desc = "Start the duration timer for the open filetype",
         once = true,
         callback = bufEnter,
     })
 
-    vim.api.nvim_create_autocmd("BufLeave", {
+    vim.api.nvim_create_autocmd("QuitPre", {
         group = augroup,
         desc = "End the duration timer for the open filetype",
         once = true,
