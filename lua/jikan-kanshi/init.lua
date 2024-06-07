50a7222efc871ca5492c8f10b2c27ea122329f9f
Stoyko Tolev <st.tolev@gmail.com>
2024-06-08 01:02:18 +0300

    chore(lint): Update linting style

--- lua/jikan-kanshi/init.lua
+++ lua/jikan-kanshi/init.lua
@@ -3,24 +3,24 @@ local Data = require("jikan-kanshi.data")
 local BufFunctions = require("jikan-kanshi.jikan-kanshi")
 
 local function setup()
-    Data:sync()
-    vim.api.nvim_create_autocmd("BufNew", {
-        group = augroup,
-        desc = "Start the duration timer for the open filetype",
-        once = true,
-        callback = function()
-            BufFunctions:bufEnter()
-        end,
-    })
+  Data:sync()
+  vim.api.nvim_create_autocmd("BufNew", {
+    group = augroup,
+    desc = "Start the duration timer for the open filetype",
+    once = true,
+    callback = function()
+      BufFunctions:bufEnter()
+    end,
+  })
 
-    vim.api.nvim_create_autocmd("QuitPre", {
-        group = augroup,
-        desc = "End the duration timer for the open filetype",
-        once = true,
-        callback = function()
-            BufFunctions:bufLeave()
-        end,
-    })
+  vim.api.nvim_create_autocmd("QuitPre", {
+    group = augroup,
+    desc = "End the duration timer for the open filetype",
+    once = true,
+    callback = function()
+      BufFunctions:bufLeave()
+    end,
+  })
 end
 
 return { setup = setup }
