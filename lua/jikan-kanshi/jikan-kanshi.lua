1c173a0561fc411692a5bd6c8b6c72fd3bbf119f
Stoyko Tolev <st.tolev@gmail.com>
2024-06-10 22:57:03 +0300

    feat(tracking): Add table with most popular languages.

--- lua/jikan-kanshi/jikan-kanshi.lua
+++ lua/jikan-kanshi/jikan-kanshi.lua
@@ -1,6 +1,58 @@
 local function checkInvalidFT(fileType)
   return fileType == nil or fileType == "" or fileType == "TelescopePrompt"
 end
+
+local fileTypeMapping = {
+  typescriptreact = "typescript",
+  typescript = "typescript",
+  javascriptreact = "javascript",
+  javascript = "javascript",
+  lua = "lua",
+  rust = "rust",
+  python = "python",
+  ruby = "ruby",
+  java = "java",
+  c = "c",
+  cpp = "cpp",
+  csharp = "csharp",
+  go = "go",
+  php = "php",
+  html = "html",
+  css = "css",
+  scss = "css",
+  less = "css",
+  json = "json",
+  xml = "xml",
+  markdown = "markdown",
+  sh = "bash",
+  bash = "bash",
+  zsh = "bash",
+  powershell = "powershell",
+  kotlin = "kotlin",
+  dart = "dart",
+  objectivec = "objective-c",
+  objc = "objective-c",
+  sql = "sql",
+  perl = "perl",
+  r = "r",
+  swift = "swift",
+  matlab = "matlab",
+  julia = "julia",
+  elixir = "elixir",
+  erlang = "erlang",
+  scala = "scala",
+  groovy = "groovy",
+  haskell = "haskell",
+  clojure = "clojure",
+  lisp = "lisp",
+  vim = "viml",
+  vimscript = "viml",
+  tex = "latex",
+  latex = "latex",
+  toml = "toml",
+  yaml = "yaml",
+}
+
 local startTime
 local endTime
 local fileType
@@ -9,7 +61,7 @@ local M = {}
 
 --- Called when entering a buffer and the tracking needs to start
 function M:bufEnter()
-  fileType = vim.bo.filetype
+  fileType = fileTypeMapping[vim.bo.filetype] or "other"
   if checkInvalidFT(fileType) then
     return
   end
